94838af01978f7657e7202b9a4e842d4404110b7
bsse0522 <bsse0522@iit.du.ac.bd>
2018-04-21 01:59:04 +0600

    halladdministration

--- resources/views/user/hallAdministration.blade.php
+++ resources/views/user/hallAdministration.blade.php
@@ -20,58 +20,58 @@
                 <div class="row">
                     <div class="team-wrapper-">
                         @foreach($administration as $item)
-                            <div class="col-md-3 col-sm-6">
-                            <div class="team-item">
-                                <div class="team-thumb">
-                                    <?php
-                                        if($item->photo){
-                                            $path =$item->photo;
-                                        }else{
-                                            $path = "propic.jpg";
-                                        }
-
-                                    ?>
-                                    <img class="img-responsive" src="/uploads/images/{{$path}}" alt="">
-                                    <ul class="team-social">
+                            <div class="col-md-3 col-sm-6" style="min-height: 440px;">
+                                <div class="team-item">
+                                    <div class="team-thumb">
                                         <?php
-                                            if($item->fb){
-                                                $fb = $item->fb;
+                                            if($item->photo){
+                                                $path =$item->photo;
+                                            }else{
+                                                $path = "propic.jpg";
                                             }
-                                            else{ $fb = "javascript:void(0);";}
-                                        if($item->twitter){
-                                            $twitter = $item->twitter;
-                                        }
-                                        else{ $twitter = "javascript:void(0);";}
-                                        if($item->rss){
-                                            $rss = $item->rss;
-                                        }
-                                        else{ $rss = "javascript:void(0);";}
-                                        if($item->linked_in){
-                                            $linked_in = $item->linked_in;
-                                        }
-                                        else{ $linked_in = "javascript:void(0);";}
 
                                         ?>
-                                        <li>
-                                            <a href="{{$fb}}"> <i class="fa fa-facebook"></i></a>
-                                        </li>
-                                        <li>
-                                            <a href="{{$twitter}}"> <i class="fa fa-twitter"></i></a>
-                                        </li>
-                                        <li>
-                                            <a href="{{$linked_in}}"> <i class="fa fa-linkedin"></i></a>
-                                        </li>
-                                        <li>
-                                            <a href="{{$rss}}"> <i class="fa fa-rss"></i></a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="team-content">
-                                    <h3>{{$item->name}}</h3>
-                                    <span class="position">{{$item->designation}}</span>
+                                        <img class="img-responsive" src="/uploads/images/{{$path}}" alt="">
+                                        <ul class="team-social">
+                                            <?php
+                                                if($item->fb){
+                                                    $fb = $item->fb;
+                                                }
+                                                else{ $fb = "javascript:void(0);";}
+                                            if($item->twitter){
+                                                $twitter = $item->twitter;
+                                            }
+                                            else{ $twitter = "javascript:void(0);";}
+                                            if($item->rss){
+                                                $rss = $item->rss;
+                                            }
+                                            else{ $rss = "javascript:void(0);";}
+                                            if($item->linked_in){
+                                                $linked_in = $item->linked_in;
+                                            }
+                                            else{ $linked_in = "javascript:void(0);";}
+
+                                            ?>
+                                            <li>
+                                                <a href="{{$fb}}"> <i class="fa fa-facebook"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="{{$twitter}}"> <i class="fa fa-twitter"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="{{$linked_in}}"> <i class="fa fa-linkedin"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="{{$rss}}"> <i class="fa fa-rss"></i></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="team-content">
+                                        <h3>{{$item->name}}</h3>
+                                        <span class="position">{{$item->designation}}</span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
                         @endforeach
                     </div>
                 </div>
